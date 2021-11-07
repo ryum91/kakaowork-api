d9b02cae5bec60b60ef17807bc7769eff4cfb99e
ryum91 <ryum91@gmail.com>
2021-11-08 01:21:47 +0900

    fix: Response Type 변경

--- src/requests/axios.ts
+++ src/requests/axios.ts
@@ -5,12 +5,14 @@ const { KAKAOWORK_BOT_API_KEY } = process.env;
 const BASE_URL = 'https://api.kakaowork.com';
 
 export const request = <T>(options?: Exclude<AxiosRequestConfig, 'baseURL' | 'headers'>) => {
-  return axios.request<T>({
-    baseURL: BASE_URL,
-    headers: {
-      Authorization: `Bearer ${KAKAOWORK_BOT_API_KEY}`,
-      'Content-Type': 'application/json'
-    },
-    ...options
-  });
+  return axios
+    .request<T>({
+      baseURL: BASE_URL,
+      headers: {
+        Authorization: `Bearer ${KAKAOWORK_BOT_API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      ...options
+    })
+    .then(({ data }) => data);
 };
